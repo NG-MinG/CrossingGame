17201ec48f16c59ed3238298f6f177ed667bd0ec
M2KV <90041846+M2KV@users.noreply.github.com>
2021-11-23 14:20:41 +0700

    new update

---
+++ CrossingRoad/TrafficLight.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "Graphics.h"
+#include "Console.h"
+
+template<class T>
+class TrafficLight
+{
+private:
+	COORD pos;
+	short greenTime, redTime;
+	short prevgreenTime, prevredTime;
+	bool isRed;
+
+public:
+	TrafficLight<T>() {};
+	TrafficLight<T>(COORD p, short gt, short rt)
+	{
+		pos = p;
+		greenTime = gt;
+		prevgreenTime = gt;
+		redTime = rt;
+		prevredTime = rt;
+		isRed = false;
+	}
+
+	void Draw()
+	{
+		if (isRed == true) Graphics::DrawTexts(Console::outputHandle(), "█", pos, Color::red);
+		else Graphics::DrawTexts(Console::outputHandle(), "█", pos, Color::green);
+	}
+
+	void UpdateState(std::vector<T*> obj)
+	{
+		if (greenTime != 0) greenTime--;
+		else
+		{
+			isRed = true;
+			if (redTime != 0) redTime--;
+			else redTime = prevredTime, greenTime = prevgreenTime, isRed = false;
+		}
+
+		Draw();
+
+		for (T* x : obj)
+			if (isRed == true) x->StopRedLight();
+			else x->RunGreenLight();
+	}
+
+};
